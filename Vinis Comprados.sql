d648f113899ad71981f1f7a924e34c62ddcaa971
Paulo Henrique de Andrade <141787223+pblcnr@users.noreply.github.com>
2023-11-28 20:21:49 -0300

    Finalizando as tabelas

---
+++ Vinis Comprados.sql
@@ -0,0 +1,26 @@
+/*
+-- Query: SELECT * FROM Lista_Vinis_Comprados
+LIMIT 0, 1000
+
+-- Date: 2023-11-28 20:21
+*/
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (1,1);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (3,2);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (1,3);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (3,4);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (1,5);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (3,6);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (1,7);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (4,8);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (6,9);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (2,10);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (4,11);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (2,12);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (6,13);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (2,14);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (5,15);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (7,16);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (5,17);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (7,18);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (9,19);
+INSERT INTO `` (`ID_Transacao`,`ID_Vinil`) VALUES (8,20);
